f5adc1644b6032befdfb26fb7f1e22628d5c1fe5
vboxuser <vboxuser@vbox.telekom.ip>
2025-11-11 21:41:20 +0100

    added camera move

--- main.c
+++ main.c
@@ -10,54 +10,57 @@ t_vec vec(double x, double y, double z)
 	return vec;
 }
 
+int	handle_key(int keycode, t_data *data)
+{
+	if (keycode == 65307)
+		exit(0);
+	else if (keycode == 'w')
+		data->pov->cam.z -= 0.2;
+	else if (keycode == 's')
+		data->pov->cam.z += 0.2;
+	else if (keycode == 'a')
+		data->pov->cam.x -= 0.2;
+	else if (keycode == 'd')
+		data->pov->cam.x += 0.2;
+	else if (keycode == ' ')
+		data->pov->cam.y += 0.2;
+	else if (keycode == 'c')
+		data->pov->cam.y -= 0.2;
+	clear_image(data->img);
+	render_scene(data);
+	return (0);
+}
+
 int main(void)
 {
-    void		*mlx = mlx_init();
-    void		*win = mlx_new_window(mlx, WIDTH, HEIGHT, "miniRT");
-	t_img img;
+	void	*mlx = mlx_init();
+	void	*win = mlx_new_window(mlx, WIDTH, HEIGHT, "miniRT");
+	t_img	img;
 	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	t_pov		pov = {
-		.cam = vec(0, 0, 0)
+	t_pov	pov = {
+		.cam = vec(0, 0, 0),
+		.fov = 90.0
 	};
 	t_shapes	shapes = {
-		.ambient = { .ratio = 0.3, .color = color(0xFFFF88)},
-		.cylinder = {.center = vec(3, 0, -5), .axis = vec_norm(vec(0, 1, 1)),
-						.radius = 1.0, .height = 3.0, .color = 0x00FF00},
-		.sphere = { vec(-3, 0, -5), 1.0, 0xFFFFFF },
-		.plane = {.center = vec(0, 0, -4), .axis = vec_norm(vec(-1, 1, 1)),
-						.color = 0x000000},
-		.light = {.pos = vec(0.0, -1.0, -1.0), .brightness = 0.6,
-						.color = color(0xAAFFFF)}
+		.ambient = { .ratio = 0.1, .color = color(0xFFFFFF)},
+		// .cylinder = {.center = vec(50, 0, 20.6), .axis = vec_norm(vec(0, 0, 1)),
+		// 		.radius = 14.2, .height = 21.42, .color = 0x00FF00},
+		.sphere = { vec(0.0, 0.0, -5.0), 3, 0xFF0000 },
+		// .plane = {.center = vec(0, 0, -10), .axis = vec_norm(vec(0, 1, 0)),
+		// 		.color = 0x0000FF},
+		.light = { .pos = vec(4, 0, 1), .brightness = 0.6,
+				.color = color(0xFFFFFF) }
 	};
-	t_hit		hit;
-	t_data		data = {
+	t_data	data = {
 		.mlx_ptr = mlx,
 		.win_ptr = win,
 		.img = &img,
 		.pov = &pov,
 		.shapes = &shapes
 	};
-
-    double		fov = 100.0;
-    double		scale = tan((fov * 0.5) * PI / 180);
-
-    for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-            pov.x = (2 * (x + 0.5) / (double)WIDTH - 1) * scale * (WIDTH / (double)HEIGHT);
-            pov.y = (1 - 2 * (y + 0.5) / (double)HEIGHT) * scale;
-            pov.dir = vec_norm(vec(pov.x, pov.y, -2));
-            if (hit_sphere(pov.cam, pov.dir, shapes.sphere, &hit))
-				render(&data, &hit, &shapes, x, y);
-			else if (hit_cylinder(pov.cam, pov.dir, shapes.cylinder, &hit))
-				render(&data, &hit, &shapes, x, y);
-			else if (hit_plane(pov.cam, pov.dir, shapes.plane, &hit))
-				render(&data, &hit, &shapes, x, y);
-            else
-                my_mlx_pixel_put(data.img, x, y, 0x888888);
-        }
-    }
-	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
-    mlx_loop(mlx);
-    return 0;
+	render_scene(&data);
+	mlx_key_hook(win, handle_key, &data);
+	mlx_loop(mlx);
+	return 0;
 }
