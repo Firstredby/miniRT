0639e2d2c2b5ae527da769fb0bfa0185458dc59b
Firstredby <imshchirov@gmail.com>
2025-11-21 17:56:53 +0100

    added prep function for camera axis

--- main.c
+++ main.c
@@ -1,34 +1,25 @@
 #include "miniRT.h"
 
-t_vec vec(double x, double y, double z)
-{
-	t_vec	vec;
-
-	vec.x = x;
-	vec.y = y;
-	vec.z = z;
-	return vec;
-}
-
 int	handle_key(int keycode, t_data *data)
 {
+	(void)data;
+	if (DEBUG_ON)
+		moving(keycode, data);
 	if (keycode == 65307)
 		exit(0);
-	else if (keycode == 'w')
-		data->pov->cam.z -= 0.2;
-	else if (keycode == 's')
-		data->pov->cam.z += 0.2;
-	else if (keycode == 'a')
-		data->pov->cam.x -= 0.2;
-	else if (keycode == 'd')
-		data->pov->cam.x += 0.2;
-	else if (keycode == ' ')
-		data->pov->cam.y += 0.2;
-	else if (keycode == 'c')
-		data->pov->cam.y -= 0.2;
-	clear_image(data->img);
-	render_scene(data);
 	return (0);
+}
+
+void	axis_prep(t_data *data)
+{
+	t_vec	world_up;
+
+	data->pov->scale = tan((data->pov->fov * 0.5) * PI / 180);
+	data->pov->aspect = (double)WIDTH / (double)HEIGHT;
+	data->pov->forward = vec_norm(data->pov->dir);
+	world_up = vec(0, 1, 0);
+	data->pov->right = vec_norm(vec_norm(vec_cross(data->pov->forward, world_up)));
+	data->pov->up = vec_cross(data->pov->right, data->pov->forward);
 }
 
 int main(void)
@@ -39,18 +30,34 @@ int main(void)
 	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	t_pov	pov = {
-		.cam = vec(0, 0, 0),
-		.fov = 90.0
+		.cam = vec(7, 0, -5.0),
+		.dir = vec_norm((t_vec){-1, 0, 0}),
+		.fov = 70.0
 	};
 	t_shapes	shapes = {
-		.ambient = { .ratio = 0.1, .color = color(0xFFFFFF)},
-		// .cylinder = {.center = vec(50, 0, 20.6), .axis = vec_norm(vec(0, 0, 1)),
-		// 		.radius = 14.2, .height = 21.42, .color = 0x00FF00},
-		.sphere = { vec(0.0, 0.0, -5.0), 3, 0xFF0000 },
-		// .plane = {.center = vec(0, 0, -10), .axis = vec_norm(vec(0, 1, 0)),
-		// 		.color = 0x0000FF},
-		.light = { .pos = vec(4, 0, 1), .brightness = 0.6,
-				.color = color(0xFFFFFF) }
+		 .ambient = { .ratio = 0.15, .color = color(0x222222) },
+		.cylinder = {
+			.center = vec(0.0, -0.5, -5.0),
+			.axis = vec_norm(vec(0.0, 1.0, 0.0)),
+			.radius = 1.0,
+			.height = 3.0,
+			.color = 0xCC4444
+		},
+		.plane = {
+			.center = vec(0.0, -2.0, -5.0),
+			.axis = vec_norm(vec(0.0, 1.0, 0.0)),
+			.color = 0x222222
+		},
+		.sphere = {
+			.center = vec(-3.0, 0.0, -6.0),
+			.radius = 0.8,
+			.color = 0x00CCFF
+		},
+		.light = {
+			.pos = vec(3.0, 4.0, -2.0),
+			.brightness = 1.0,
+			.color = color(0xFFFFFF)
+		}
 	};
 	t_data	data = {
 		.mlx_ptr = mlx,
@@ -60,7 +67,7 @@ int main(void)
 		.shapes = &shapes
 	};
 	render_scene(&data);
-	mlx_key_hook(win, handle_key, &data);
+	mlx_key_hook(win, &handle_key, &data);
 	mlx_loop(mlx);
 	return 0;
 }
